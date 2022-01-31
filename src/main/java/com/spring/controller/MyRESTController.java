43c63b32b0cc0e452235411824ccbbaa34186f3b
Aleksey Vasilenko <vasilenko.aleksey.v@gmail.com>
2022-01-31 18:17:39 +0200

    Initial commit

---
+++ src/main/java/com/spring/controller/MyRESTController.java
@@ -0,0 +1,22 @@
+package com.spring.controller;
+import com.spring.entity.Employee;
+import com.spring.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees(){
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+}
